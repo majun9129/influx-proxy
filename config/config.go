205ead64024212aa2c2162b4974730cbbd895b50
chengshiwen <chengshiwen0103@gmail.com>
2020-04-08 17:51:26 +0800

    add HttpTimeout config

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 const (
     Version            = "2.4.4"
     IdleTimeout        = 10
+    HttpTimeout        = 10
     CheckPingInterval  = 1
     WaitActiveInterval = 10
     RewriteInterval    = 10
